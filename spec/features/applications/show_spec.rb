c8cd0511852158c8e5189bda3c9f5fc1bd18491a
Matt Toensing <matthew.toensing@gmail.com>
2021-07-17 12:58:01 -0700

    Add expectations to existing test for form not being there when description is completed

--- spec/features/applications/show_spec.rb
+++ spec/features/applications/show_spec.rb
@@ -110,19 +110,10 @@ RSpec.describe 'application show page'  do
     fill_in "description", with: "I Love Animals!"
     click_on "Submit Description"
 
-    expect(application.status).to eq("Pending")
+    expect(current_path).to eq("/applications/#{application.id}")
     expect(page).to have_content("Pending")
-
-    # As a visitor
-    # When I visit an application's show page
-    # And I have added one or more pets to the application
-    # Then I see a section to submit my application
-    # And in that section I see an input to enter why I would make a good owner for these pet(s)
-    # When I fill in that input
-    # And I click a button to submit this application
-    # Then I am taken back to the application's show page
-    # And I see an indicator that the application is "Pending"
-    # And I see all the pets that I want to adopt
-    # And I do not see a section to add more pets to this application
+    expect(page).to have_content("Pets Applied For: #{pet.name}")
+    expect(page).to_not have_button('Submit')
+    expect(page).to_not have_content("Add a Pet to this Application")
   end
 end
