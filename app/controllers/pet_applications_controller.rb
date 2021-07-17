64890d81ad1b98d4119918892db5eca4fbc3ba9c
Matt Toensing <matthew.toensing@gmail.com>
2021-07-17 07:56:44 -0700

    Change order of instance variables

--- app/controllers/pet_applications_controller.rb
+++ app/controllers/pet_applications_controller.rb
@@ -1,6 +1,8 @@
 
 class PetApplicationsController < ApplicationController
   def update
+    @application = Application.find(params[:application_id])
+    @pet = Pet.find(params[:pet_id])
     @petapplication = PetApplication.create!(application_id: params[:application_id], pet_id: params[:pet_id])
   end
 end
