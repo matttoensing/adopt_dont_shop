211c786e7dd92bfde3b3a2c94058a60d8b414288
Matt Toensing <matthew.toensing@gmail.com>
2021-07-16 22:43:01 -0700

    Add link and action for pet applications

--- app/controllers/pet_applications_controller.rb
+++ app/controllers/pet_applications_controller.rb
@@ -1,4 +1,6 @@
 
-class PetApplications < ApplicationController
-  
+class PetApplicationsController < ApplicationController
+  def update
+    @petapplication = PetApplication.create!(application_id: params[:application_id], pet_id: params[:pet_id])
+  end
 end
