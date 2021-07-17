f1a66ec8c8c9af7f30c3ef225e0e3b3bb9addd99
Matt Toensing <matthew.toensing@gmail.com>
2021-07-17 11:59:23 -0700

    Add change status method

--- app/models/application.rb
+++ app/models/application.rb
@@ -11,10 +11,7 @@ class Application < ApplicationRecord
   has_many :pet_applications
   has_many :pets, through: :pet_applications
 
-  def self.pets_on_application(petid)
-    # Application.select('pets.*').joins(:pets).where('pet_id = ?', petid)
-    # Application.find_by_sql(Pet.joins(:applications).where('pet_id = ?', petid).select("DISTINCT pets.*").to_sql)
-    # Application.left_joins(:pets).where('pet_applications.pet_id = ?', petid).select('pets.*')
-    Application.joins(:pets).where('pet_id = ?', petid).select('pets.*')
+  def change_status_pending
+    self.status = "Pending"
   end
 end
