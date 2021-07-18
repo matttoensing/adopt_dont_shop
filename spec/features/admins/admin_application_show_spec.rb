05388108632e5afffb7a779ee2082e95c126e236
Matt Toensing <matthew.toensing@gmail.com>
2021-07-18 10:48:17 -0700

    Add tests to include having pets on different admin application show pages

--- spec/features/admins/admin_application_show_spec.rb
+++ spec/features/admins/admin_application_show_spec.rb
@@ -62,14 +62,35 @@ RSpec.describe 'admin application show page' do
     expect(page).to have_content("Status: Rejected")
     expect(page).to have_content("#{pet1.name} Rejected")
   end
-end
 
-# Rejecting a Pet for Adoption
-#
-# As a visitor
-# When I visit an admin application show page ('/admin/applications/:id')
-# For every pet that the application is for, I see a button to reject the application for that specific pet
-# When I click that button
-# Then I'm taken back to the admin application show page
-# And next to the pet that I rejected, I do not see a button to approve or reject this pet
-# And instead I see an indicator next to the pet that they have been rejected
+  it 'clicking reject on one application does not affect other applications for the same pet' do
+    shelter = Shelter.create!(name: 'Aurora Shelter', city: 'Aurora, CO', foster_program: true, rank: 5)
+    application1 = create(:application, status: "Pending")
+    application2 = create(:application, status: "Pending")
+    application3 = create(:application, status: "Pending")
+    pet1 = create(:pet, shelter_id: shelter.id)
+    pet2 = create(:pet, shelter_id: shelter.id)
+    petapp1 = PetApplication.create!(application_id: application1.id, pet_id: pet1.id)
+    petapp2 = PetApplication.create!(application_id: application1.id, pet_id: pet2.id)
+    petapp3 = PetApplication.create!(application_id: application2.id, pet_id: pet1.id)
+    petapp4 = PetApplication.create!(application_id: application3.id, pet_id: pet2.id)
+
+    visit "/admin/applications/#{application1.id}"
+
+    click_on "Reject #{pet1.name} Adoption Request"
+
+    expect(page).to have_content("#{pet1.name} Rejected")
+
+    visit "/admin/applications/#{application2.id}"
+
+    expect(page).to have_content("Application Status: Pending")
+    expect(page).to have_button("Approve #{pet1.name}")
+    expect(page).to have_button("Reject #{pet1.name} Adoption Request")
+
+    visit "/admin/applications/#{application3.id}"
+
+    expect(page).to have_content("Application Status: Pending")
+    expect(page).to have_button("Approve #{pet2.name}")
+    expect(page).to have_button("Reject #{pet2.name} Adoption Request")
+  end
+end
