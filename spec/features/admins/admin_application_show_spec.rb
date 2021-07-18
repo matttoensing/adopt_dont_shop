57dffaa64605d499894e2cd2859854ac64e78fce
Matt Toensing <matthew.toensing@gmail.com>
2021-07-17 18:51:29 -0700

    Include test for rejection button on admin page

--- spec/features/admins/admin_application_show_spec.rb
+++ spec/features/admins/admin_application_show_spec.rb
@@ -31,14 +31,28 @@ RSpec.describe 'admin application show page' do
     expect(page).to_not have_button("Approve #{pet1.name}")
     expect(page).to have_button("Approve #{pet2.name}")
   end
-end
 
-#  Approving a Pet for Adoption
+  it 'has a reject button next to each pet before approval' do
+    shelter = Shelter.create!(name: 'Aurora Shelter', city: 'Aurora, CO', foster_program: true, rank: 5)
+    application = create(:application, status: "Pending")
+    pet1 = create(:pet, shelter_id: shelter.id)
+    pet2 = create(:pet, shelter_id: shelter.id)
+    petapp1 = PetApplication.create!(application_id: application.id, pet_id: pet1.id)
+    petapp2 = PetApplication.create!(application_id: application.id, pet_id: pet2.id)
+
+    visit "/admin/applications/#{application.id}"
+
+    expect(page).to have_button("Reject #{pet1.name} Adoption Request")
+    expect(page).to have_button("Reject #{pet2.name} Adoption Request")
+
+# Rejecting a Pet for Adoption
 #
 # As a visitor
 # When I visit an admin application show page ('/admin/applications/:id')
-# For every pet that the application is for, I see a button to approve the application for that specific pet
+# For every pet that the application is for, I see a button to reject the application for that specific pet
 # When I click that button
 # Then I'm taken back to the admin application show page
-# And next to the pet that I approved, I do not see a button to approve this pet
-# And instead I see an indicator next to the pet that they have been approved
+# And next to the pet that I rejected, I do not see a button to approve or reject this pet
+# And instead I see an indicator next to the pet that they have been rejected
+  end
+end
