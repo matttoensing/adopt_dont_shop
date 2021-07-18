2585bbf5eafd6ec0a3da7f08647a63c10fb8634a
Matt Toensing <matthew.toensing@gmail.com>
2021-07-17 17:20:20 -0700

    Add show action with application and application pets instance variables

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -4,4 +4,9 @@ class AdminsController < ApplicationController
     @shelters = Shelter.order_in_reverse
     @shelters_with_pends = Shelter.shelters_with_pending_apps
   end
+
+  def show
+    @application = Application.find(params[:id])
+    @pets = @application.pets
+  end
 end
