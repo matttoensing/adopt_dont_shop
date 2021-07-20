1660269058168f2799139d7b09266af5f9a600fc
Matt Toensing <matthew.toensing@gmail.com>
2021-07-20 13:14:14 -0700

    Delete unneeded action

--- app/controllers/pet_applications_controller.rb
+++ app/controllers/pet_applications_controller.rb
@@ -7,14 +7,4 @@ class PetApplicationsController < ApplicationController
     @petapplication = PetApplication.create!(application_id: params[:application_id], pet_id: params[:pet_id])
     redirect_to(controller: 'applications', action: 'show', id: @application.id, pet: @pet.name)
   end
-
-  # def delete
-  #   require "pry"; binding.pry
-  #   @application = Application.find(params[:application_id])
-  #   @application_rejected = Application.find(params[:application_id])
-  #   @application_rejected.change_status_rejected
-  #   @pet_rejected = Pet.find(params[:pet_id])
-  #   @pet_application = PetApplication.find(params[:id])
-  #   redirect_to(controller: 'applications', action: 'show', id: @application.id, pet: @pet.name, reject: "true")
-  # end
 end
