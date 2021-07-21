8527ef19188250373aa39f9caa1529c4967b0713
Matt Toensing <matthew.toensing@gmail.com>
2021-07-20 17:51:23 -0700

    Change things around in the update action

--- app/controllers/pet_applications_controller.rb
+++ app/controllers/pet_applications_controller.rb
@@ -1,10 +1,11 @@
 
 class PetApplicationsController < ApplicationController
   def update
-    @application = Application.find(params[:application_id])
-    @application.change_status_pending
-    @pet = Pet.find(params[:pet_id])
-    @petapplication = PetApplication.create!(application_id: params[:application_id], pet_id: params[:pet_id])
-    redirect_to(controller: 'applications', action: 'show', id: @application.id, pet: @pet.name)
+    application = Application.find(params[:application_id])
+    application.change_status_pending
+    pet = Pet.find(params[:pet_id])
+    petapplication = PetApplication.create!(application_id: params[:application_id], pet_id: params[:pet_id], status: "Pending")
+
+    redirect_to(controller: 'applications', action: 'show', id: application.id, pet: pet.name)
   end
 end
